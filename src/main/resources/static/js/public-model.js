f81d9a71fbf2ca556116a2e2a76b01d7f09eb015
王鹏飞 <wpf52sh@163.com>
2018-11-03 12:30:09 +0800

    回到顶部ok

--- src/main/resources/static/js/public-model.js
+++ src/main/resources/static/js/public-model.js
@@ -1,5 +1,5 @@
+/*查询视频*/
 function PublicF(page,list) {
-
     var pageUtil = null;
     $.ajax({
         url:'/dianying/dianyings',
@@ -12,4 +12,18 @@ function PublicF(page,list) {
         }
     })
     return pageUtil;
+}
+
+/*返回顶部*/
+$(function(){
+    $("#toTop").click(function(){
+        var odiv=document.getElementById('head');
+        $("html").animate({"scrollTop": odiv.offsetTop},500); //IE,FF
+        $("body").animate({"scrollTop": odiv.offsetTop},500); //Webkit
+    });
+})
+
+/*模糊搜索*/
+function solr() {
+    $("")
 }
